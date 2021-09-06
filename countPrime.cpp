7ff6fb2270f41d4ad4fa5343c3ddb6fe1908fbdb
trikohung <lamgiahung@gmail.com>
2021-09-06 11:56:45 +0700

    count prime numbers

---
+++ countPrime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+int countPrimes(int n) {
+    vector<bool> prime (n, true);
+    int count = 0;
+    if (n <= 2) return 0;
+
+    for (int i = 2; i < sqrt(n); i++) {
+        if (prime[i]) {
+            for (int j = i*i; j < n; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (prime[i]) count++;
+    }
+    return count;
+}
+
+int main() {
+    cout << countPrimes(10);
+}
